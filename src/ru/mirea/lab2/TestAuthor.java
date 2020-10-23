c83420982e537794fd27a504a12a2ee0b92171ff
akihabara0318 <mazilok00957@gmail.com>
2020-10-23 19:25:03 +0300

    Initial commit

---
+++ src/ru/mirea/lab2/TestAuthor.java
@@ -0,0 +1,8 @@
+package ru.mirea.lab2;
+
+public class TestAuthor {
+    public static void main(String[] args) {
+        Author a1 = new Author("Леы Толстой", "утонул@mail.ru", 'M');
+        System.out.println(a1);
+    }
+}
